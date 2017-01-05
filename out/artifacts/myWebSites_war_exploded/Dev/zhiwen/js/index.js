ae30b38cc415b7137cb4e9ca4f3f25996512f0e2
liu_bo_shuo <ls132981@hotmail.com>
2017-01-05 22:32:18 +0800

    提交

--- out/artifacts/myWebSites_war_exploded/Dev/zhiwen/js/index.js
+++ out/artifacts/myWebSites_war_exploded/Dev/zhiwen/js/index.js
@@ -8,4 +8,32 @@ $(function () {
     $(".question_button").button();
 
 
+
+
+    $("#reg").buttonset();
+    $("#birthday").datepicker();
+    $("#reg input[title]").tooltip();
+
+    $("#reg_a").click(function () {
+
+        $("#reg").dialog({
+
+            autoOpen:true,
+            modal:true,
+            width:320,
+            height:340,
+            resizable:false,
+            buttons:{
+                "提交":function(){
+                    $("#reg").dialog("destroy");
+                }
+            }
+
+        });
+        //d
+        // $("#reg").dialog("open");
+
+    })
+
+
 });
