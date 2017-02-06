0101d042448e8735ec85e00da02d1668f896df12
liu_bo_shuo <ls132981@hotmail.com>
2017-02-06 20:58:54 +0800

    validate.js
    练习

--- out/artifacts/myWebSites_war_exploded/zhiwen/js/index.js
+++ out/artifacts/myWebSites_war_exploded/zhiwen/js/index.js
@@ -11,7 +11,33 @@ $(function () {
 
 
     $("#reg").buttonset();
-    $("#birthday").datepicker();
+    $("#birthday").datepicker({
+        dateFormat:"yy-mm-dd",
+        // dayNames:["星期日","星期一","星期二","星期三","星期四","星期五","星期六"],
+        // dayNamesShort:["星期日","星期一","星期二","星期三","星期四","星期五","星期六"],
+
+        dayNamesMin:["日","一","二","三","四","五","六"],
+        monthNames:["一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月"],
+        monthNamesShort:["一","二","三","四","五","六","七","八","九","十","十一","十二"],
+
+        // appendText:"日历",
+        // showWeek:true,
+        // weekHeader:"周",
+
+        firstDay:1,
+
+        changeMonth: true,
+        changeYear: true,
+        prevText:"上一月",
+        nextText:"下一月",
+        yearSuffix:"年",
+        showMonthAfterYear:true,
+
+        maxDate:0,
+        minDate:-8000,//优先级最高
+        hideIfNoPrevNext: true,
+        yearRange:"1950:2020", // 优先级低
+    });
     // $("#reg input[title]").tooltip();
 
     $("#reg_a").click(function () {
@@ -39,7 +65,7 @@ $(function () {
             delay:0,
             autoFocus: true,
             source: function(request,response) {
-                
+
                 var hosts = ["qq.com","163.com","sina.com.cn","gmail.com","hotmail.com"];
                 var term = request.term;
                 var name = term;
